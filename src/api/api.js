d9f32f21bf80caeff4fbe8b13a9e4cb1f7d740e0
unknown <quynhnhutg6@gmail.com>
2023-02-15 16:18:49 +0700

    up scroll to top

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,9 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: 'https://latata-shop-app.onrender.com',
+    // baseURL: 'https://latata-shop-app.onrender.com',
+    baseURL: 'https://localhost:4000',
+     
 
 })
 
