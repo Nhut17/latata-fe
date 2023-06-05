eca33de2bbadd9058785b2b82bc8b7f2c5cab59e
bichtram67 <tramnguyen672001@gmail.com>
2023-06-05 22:44:18 +0700

    update api

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
 
     // baseURL: 'http://localhost:4000',
     // baseURL: 'https://latata-shop-app.onrender.com', 
-    baseURL: 'http://latata-shop.herokuapp.com',
+    baseURL: 'https://latata-shop.herokuapp.com',
 
 
 })
